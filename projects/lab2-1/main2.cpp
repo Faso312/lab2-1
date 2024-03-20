e8b8ba0fb701e301404ec4ac4cd7c7d6a2dd3223
Faso312 <faso.312@yandex.ru>
2024-03-20 13:12:33 +0400

    13-12_20-03

--- projects/lab2-1/main2.cpp
+++ projects/lab2-1/main2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 struct Date {
     int day;
@@ -10,7 +11,7 @@ struct Date {
 struct Student {
     std::string surname;
     std::string name;
-    char gender;  // 'M' - мужской, 'F' - женский
+    char gender;
     Date birthDate;
 };
 
@@ -21,12 +22,18 @@ int main() {
         {"Фамилия3", "Имя3", 'M', {10, 12, 2001}},
         {"Фамилия4", "Имя4", 'F', {20, 3, 2002}}
     };
+    
+    // Получение текущей даты
+    auto now = std::chrono::system_clock::now();
+    time_t now_c = std::chrono::system_clock::to_time_t(now);
+    tm* current_time = std::localtime(&now_c);
+    int currentYear = current_time->tm_year + 1900; // tm_year содержит текущий год с 1900 года
 
     std::cout << "Студенты, подлежащие призыву:" << std::endl;
     for (const auto& student : students) {
         if (student.gender == 'M') {
-            int currentYear = 2024;  // текущий год
             int age = currentYear - student.birthDate.year;
             if (age >= 18) {
                 std::cout << student.surname << std::endl;}}}
-    return 0;}
+    return 0;
+}
